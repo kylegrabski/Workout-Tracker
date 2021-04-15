25e0ab9c4e24db1c6037daf20a0111e302ec2a20
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-14 19:48:48 -0700

    init commit

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const path = require("path");
+const Workout = require("../models/workout");
+
+// get all workouts
+router.get("/api/workouts", (req, res) => {
+})
+
+// FIND ONE and update a workout /api/workouts/:id
+
+// POST ONE workout /api/workouts
+
+//GET WORKOUT /api/workouts/range
+
+module.exports = router;
+
