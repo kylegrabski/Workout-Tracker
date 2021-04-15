93d361630fe5dd2b70ba5b6dd869b64d0881e5f2
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-15 10:03:37 -0700

    update route debugging

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const Workout = require("../models/workout");
 
 // get all workouts
 router.get("/api/workouts", (req, res) => {
+  console.log(" GET /api/workouts HIT HERE");
   Workout.find({})
     // .populate("exercises")
     .then((workoutdb) => {
@@ -16,25 +17,31 @@ router.get("/api/workouts", (req, res) => {
 
 // FIND ONE and update a workout /api/workouts/:id
 router.put("/api/workouts/:id", (req, res) => {
+  console.log(" PUT /api/workouts/:id HIT HERE");
+  console.log(" PUT /api/workouts/:id HIT HERE WITH REQ", res);
   Workout.findOneAndUpdate(
     {
       _id: req.params.id,
     },
     {
       $set: {
-        type: req.body.type,
-        name: req.body.name,
-        weight: req.body.weight,
-        sets: req.body.sets,
-        reps: req.body.reps,
-        duration: req.body.duration,
-        distance: req.body.distance,
+        exercises: {
+          type: req.body.type,
+          name: req.body.name,
+          weight: req.body.weight,
+          sets: req.body.sets,
+          reps: req.body.reps,
+          duration: req.body.duration,
+          distance: req.body.distance,
+        },
       },
     },
     (error, data) => {
       if (error) {
+        console.log("ERROR AT PUT", error);
         res.send(error);
       } else {
+        console.log("SUCCESS AT PUT", data);
         res.send(data);
       }
     }
@@ -43,16 +50,9 @@ router.put("/api/workouts/:id", (req, res) => {
 
 // POST ONE workout /api/workouts
 router.post("/api/workouts", ({ body }, res) => {
+  console.log("POST /api/workouts HIT HERE");
   Workout.create(body)
-    .then(({ _id }) =>
-      Workout.findOneAndUpdate(
-        {},
-        {
-          $push: { exercises: _id },
-        },
-        { new: true }
-      )
-    )
+
     .then((workoutdb) => {
       res.json(workoutdb);
     })
@@ -62,5 +62,6 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 //GET WORKOUT /api/workouts/range
+router.get("/api/workouts/range", (req, res) => {});
 
 module.exports = router;
